004bee1a0475aaa6a36fde4784b512fea823290f
Tapomoy Sarkar <sarkartapomoy12nov@gmail.com>
2026-03-28 19:23:07 +0530

    Create test_logic.py

---
+++ tests/test_logic.py
@@ -0,0 +1,9 @@
+# test_logic.py
+def test_simple_math():
+    # A very basic test to prove pytest is working
+    assert 2 + 2 == 4
+
+def test_snake_starting_length():
+    # Example: If you have a variable for snake length in your game
+    starting_length = 3
+    assert starting_length == 3
